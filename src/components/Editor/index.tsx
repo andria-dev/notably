83180f91fb8405e3777b31ec0d3f69650a7e4b65
Christopher Brown <chbphone55@gmail.com>
2019-03-10 11:24:42 -0600

    switch Editor to use inputHandler

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,52 +1,28 @@
-import { Editor as DraftEditor, EditorState } from 'draft-js';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { useDebouncedCallback } from 'use-debounce';
+import { Editor as DraftEditor } from 'draft-js';
+import React, { useEffect, useMemo, useState } from 'react';
 import { updateState, useStore } from '../../store';
+import { inputHandler } from './inputHandler';
 
 function Editor() {
-  const [state, dispatch] = useStore();
+  const [state] = useStore();
 
   const id = state.activeNoteID;
   const note = state.notes[id];
 
-  const idleCallbackID = useRef(-1);
   const [editorState, setEditorState] = useState(note.state);
-  const editorStateRef = useRef(editorState);
+  const [setter, unmounted] = useMemo(() => inputHandler(2000, id, updateState, true), [id]);
 
-  const saveEditorState = useDebouncedCallback(
-    async (value: EditorState) => {
-      // @ts-ignore
-      idleCallbackID.current = requestIdleCallback(
-        () => {
-          updateState(id, value, true).then(dispatch);
-        },
-        { timeout: 5000 }
-      );
-    },
-    2000,
-    [id]
+  useEffect(() => unmounted, []);
+
+  return (
+    <DraftEditor
+      editorState={editorState}
+      onChange={newState => {
+        setEditorState(newState);
+        setter(newState);
+      }}
+    />
   );
-
-  const handleEditorStateChange = useCallback(
-    (value: EditorState) => {
-      editorStateRef.current = value;
-      setEditorState(value);
-      saveEditorState(value);
-    },
-    [setEditorState, saveEditorState]
-  );
-
-  useEffect(() => {
-    return () => {
-      if (editorStateRef.current !== note.state) {
-        // @ts-ignore
-        cancelIdleCallback(idleCallbackID.current);
-        updateState(id, editorStateRef.current).then(dispatch);
-      }
-    };
-  }, []);
-
-  return <DraftEditor editorState={editorState} onChange={handleEditorStateChange} />;
 }
 
 export default Editor;
