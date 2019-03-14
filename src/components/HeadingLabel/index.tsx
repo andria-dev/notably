67d7e906476f724768efbba28f72cf4de70f30bf
Christopher Brown <chbphone55@gmail.com>
2019-03-13 19:45:45 -0600

    import styles for HeadingLabel

--- src/components/HeadingLabel/index.tsx
+++ src/components/HeadingLabel/index.tsx
@@ -1,6 +1,8 @@
 import classNames from '@chbphone55/classnames';
 import React from 'react';
 
+import './style.css';
+
 export interface IHeadingLabelProps {
   size: 1 | 2 | 3 | 4 | 5 | 6;
   [s: string]: any;
