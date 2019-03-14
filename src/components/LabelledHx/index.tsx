d5b3ae7b5a922dc09d547f3c19cac6c4ce4b3134
Christopher Brown <chbphone55@gmail.com>
2019-03-14 16:23:32 -0600

    Move LabelledHx to components folder

--- src/components/LabelledHx/index.tsx
+++ src/components/LabelledHx/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import HeadingLabel from '../../../HeadingLabel';
-import Hx from '../../../Hx';
 
 import { ContentState } from 'draft-js';
+import HeadingLabel from '../HeadingLabel';
+import Hx from '../Hx';
 import './style.css';
 
 interface ILabelledHxProps {
@@ -17,15 +17,4 @@ export function LabelledHx({ size, labelProps, ...props }: ILabelledHxProps) {
       <Hx size={size} {...props} />
     </span>
   );
-}
-
-export function headerX(size: 1 | 2 | 3 | 4 | 5 | 6) {
-  return ({ isContent = true, ...props }) => {
-    if (!isContent) {
-      return <LabelledHx size={size} {...props} />;
-    }
-
-    const { block, contentState } = props;
-    return <LabelledHx size={size}>{contentState.getPlainText()}</LabelledHx>;
-  };
 }
