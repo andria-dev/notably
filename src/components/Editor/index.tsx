53396ac1c3f8b7d71b5cc91339d41a1a1d11e46f
Christopher Brown <chbphone55@gmail.com>
2019-03-15 17:27:42 -0600

    yeet

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,10 +1,10 @@
-import { Editor as DraftEditor, EditorState, RichUtils } from 'draft-js';
+import { Editor as DraftEditor, EditorState, Modifier, RichUtils, SelectionState } from 'draft-js';
 import React, { useCallback, useEffect, useMemo, useReducer } from 'react';
 import { inputHandler } from '../../inputHandler';
 import { IAction, updateState, useStore } from '../../store';
 
 import Controls from './Controls';
-import { generateHandleKeyCommand, keyBindingFn, styleMap } from './rich-style';
+import { generateKeyBindingFn, styleMap } from './rich-style';
 import './style.css';
 
 function EditorStateReducer(state: EditorState, action: IAction): EditorState {
@@ -12,7 +12,25 @@ function EditorStateReducer(state: EditorState, action: IAction): EditorState {
     case 'change':
       return action.payload;
     case 'inline':
-    // return RichUtils.toggleInlineStyle(state, action.payload);
+      const {
+        key,
+        start = state.getSelection().getStartOffset(),
+        end = state.getSelection().getEndOffset(),
+        style
+      } = action.payload;
+
+      const content = state.getCurrentContent();
+      const selection = new SelectionState({
+        anchorKey: key,
+        anchorOffset: start,
+        focusKey: key,
+        focusOffset: end,
+        isBackward: false,
+        hasFocus: false
+      });
+
+      const newContent = Modifier.applyInlineStyle(content, selection, style);
+      return EditorState.push(state, newContent, 'change-inline-style');
     case 'block':
       return RichUtils.toggleBlockType(state, action.payload);
     default:
@@ -35,26 +53,37 @@ function Editor() {
     () => inputHandler<EditorState>(2000, id, updateState, true),
     [id]
   );
+  const handleChange = useCallback(
+    newState => {
+      dispatch({ type: 'change', payload: newState });
+      setter(newState);
+    },
+    [dispatch, setter]
+  );
 
   useEffect(() => {
     dispatch({ type: 'change', payload: note.state });
     return unmounted;
   }, [id]);
 
-  const handleKeyCommand = useCallback(generateHandleKeyCommand(dispatch), [dispatch]);
+  const keyBindingFn = useCallback(generateKeyBindingFn(editorState, dispatch), [dispatch]);
 
   return (
     <div className="Editor">
       <Controls editorState={editorState} dispatch={dispatch} />
       <DraftEditor
         editorState={editorState}
-        onChange={newState => {
-          dispatch({ type: 'change', payload: newState });
-          setter(newState);
-        }}
+        onChange={handleChange}
         customStyleMap={styleMap}
         keyBindingFn={keyBindingFn}
-        handleKeyCommand={handleKeyCommand}
+        handleKeyCommand={(command: string, currentState: EditorState) => {
+          const newState = RichUtils.handleKeyCommand(editorState, command);
+          if (newState) {
+            handleChange(newState);
+            return 'handled';
+          }
+          return 'not-handled';
+        }}
       />
     </div>
   );
--- src/components/Editor/rich-style.tsx
+++ src/components/Editor/rich-style.tsx
@@ -3,7 +3,8 @@ import Draft, {
   EditorState,
   getDefaultKeyBinding,
   ContentBlock,
-  ContentState
+  ContentState,
+  RichUtils
 } from 'draft-js';
 import React, { KeyboardEvent } from 'react';
 import { IAction } from '../../store';
@@ -32,30 +33,72 @@ export const styleMap = {
   }
 };
 
-export function keyBindingFn(event: KeyboardEvent<{}>): string | null {
-  switch (event.key) {
-    case '*':
-      return 'bold';
-    default:
-      return getDefaultKeyBinding(event);
-  }
+const isMac = navigator.platform.includes('Mac');
+export function generateKeyBindingFn(editorState: EditorState, dispatch: React.Dispatch<IAction>) {
+  return function keyBindingFn(event: KeyboardEvent<{}>): string | null {
+    switch (event.key) {
+      case '*':
+        const matches = findAllMatchesInCurrentLine(/\*.+?\*/g, '*', editorState);
+        if (matches.indices.length) {
+          for (const [start, end] of matches.indices) {
+            dispatch({
+              type: 'inline',
+              payload: {
+                key: matches.key,
+                start,
+                end,
+                style: 'BOLD'
+              }
+            });
+          }
+          return 'handled';
+        }
+      case 'b':
+        if ((isMac && event.metaKey) || (!isMac && event.ctrlKey)) {
+          dispatch({
+            type: 'inline',
+            payload: {
+              style: 'bold'
+            }
+          });
+          return null;
+        }
+      default:
+        break;
+    }
+
+    const defaultBinding = getDefaultKeyBinding(event);
+    if (
+      defaultBinding &&
+      defaultBinding in
+        ['delete', 'delete-word', 'backspace', 'backspace-word', 'backspace-to-start-of-line']
+    ) {
+      // run checks
+    }
+    return defaultBinding;
+  };
 }
 
-function* findAllMatches(regex: RegExp, text: string): IterableIterator<[number, number]> {
+function findAllMatches(regex: RegExp, text: string): Array<[number, number]> {
+  const matches: Array<[number, number]> = [];
   let match = regex.exec(text);
   while (match) {
     const start = match.index;
-    yield [start, start + match[0].length];
+    matches.push([start, start + match[0].length]);
 
     match = regex.exec(text);
   }
+  return matches;
 }
 
 function findAllMatchesInCurrentLine(
   regex: RegExp,
   input: string,
   editorState: EditorState
-): Array<[number, number]> {
+): {
+  indices: Array<[number, number]>;
+  key: string;
+} {
   const selection = editorState.getSelection();
 
   const blockKey = selection.getStartKey();
@@ -70,23 +113,10 @@ function findAllMatchesInCurrentLine(
   const previousText = contentBlock.getText();
   const text = previousText.slice(0, start) + input + previousText.slice(end);
 
-  // @ts-ignore
-  return [...findAllMatches(regex, text)];
-}
-
-export function generateHandleKeyCommand(dispatch: React.Dispatch<IAction>) {
-  return (command: string, editorState: EditorState): DraftHandleValue => {
-    switch (command) {
-      case 'bold':
-        const matches = findAllMatchesInCurrentLine(/\*.+?\*/g, '*', editorState);
-        if (matches) {
-          debugger;
-          return 'handled';
-        }
-        break;
-      default:
-        break;
-    }
-    return 'not-handled';
+  return {
+    indices: findAllMatches(regex, text).map(
+      ([s, e]): [number, number] => [s, e > end ? e - input.length : e]
+    ),
+    key: blockKey
   };
 }
