b1f6c76d43efa23c8b93fa9c1bbeef614ec76cd9
Christopher Brown <chbphone55@gmail.com>
2019-03-19 18:22:20 -0600

    edit theme meta tags on dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,25 @@ import { getNotes, ReduxProvider, store } from './store';
 /* Global CSS + Utilities */
 import './App.css';
 
+const themeMetaTags: NodeListOf<HTMLMetaElement> = document.querySelectorAll(`
+  meta[name="theme-color"],
+  meta[name="msapplication-navbutton-color"],
+  meta[name="apple-mobile-web-app-status-bar-style"]
+`);
+
 function App() {
   useDarkMode(false, {
     onChange: (isDark?: boolean) => {
       if (isDark) {
         document.body.classList.add('dark-mode');
+        themeMetaTags.forEach((tag: HTMLMetaElement) => {
+          tag.content = '#191919';
+        });
       } else {
         document.body.classList.remove('dark-mode');
+        themeMetaTags.forEach((tag: HTMLMetaElement) => {
+          tag.content = '#013b98';
+        });
       }
     }
   });
