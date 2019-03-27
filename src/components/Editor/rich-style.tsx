4af65e866a6091a282fef433507c9352d37ba345
Christopher Brown <chbphone55@gmail.com>
2019-03-27 12:29:23 -0600

    create onSpace and add docs

--- src/components/Editor/rich-style.tsx
+++ src/components/Editor/rich-style.tsx
@@ -17,6 +17,9 @@ export const plugins = [
   })
 ];
 
+/**
+ * Renders different nodes as React elements for Slate based on their node-type.
+ */
 export function renderNode(props: any, editor: Editor, next: CallableFunction) {
   const { node, attributes, children } = props;
 
@@ -42,6 +45,66 @@ export function renderNode(props: any, editor: Editor, next: CallableFunction) {
   }
 }
 
+/**
+ * Returns the block-type for any block-shortcut.
+ */
+function getType(chars: string) {
+  switch (chars) {
+    case '*':
+    case '-':
+      return 'unordered-list';
+    case '1.':
+      return 'ordered-list';
+    case '>':
+      return 'block-quote';
+    case '#':
+      return 'heading-one';
+    case '##':
+      return 'heading-two';
+    case '###':
+      return 'heading-three';
+    case '####':
+      return 'heading-four';
+    case '#####':
+      return 'heading-five';
+    case '######':
+      return 'heading-six';
+    default:
+      return null;
+  }
+}
+
+/**
+ * On space, if it was after a block-shortcut (i.e. `> ` is a blockquote),
+ * convert the current node into the shortcut's corresponding type.
+ */
+function onSpace(event: Event, editor: Editor, next: () => any) {
+  const selection = editor.value.selection;
+  if (selection.isExpanded) {
+    return next();
+  }
+
+  const startBlock = editor.value.startBlock;
+  const type = getType(startBlock.text.slice(0, selection.start.offset).replace(/\s*/g, ''));
+  if (!type || (type === 'unordered-list' && startBlock.type === 'unordered-list')) {
+    return next();
+  }
+
+  event.preventDefault();
+
+  if (type.includes('list')) {
+    editor.wrapBlock(type);
+    editor.setBlocks('list-item');
+  } else {
+    editor.setBlocks(type);
+  }
+
+  editor.moveFocusToStartOfNode(startBlock).delete();
+}
+
+/**
+ * Renders different marks as React elements based on their mark-types.
+ */
 export function renderMark(props: any, editor: Editor, next: () => any) {
   const { mark, attributes, children } = props;
 
@@ -82,6 +145,11 @@ const isCodeHotkey = isKeyHotkey('mod+j');
 const isDeletedHotkey = isKeyHotkey('mod+shift+backspace');
 const isInsertedHotkey = isKeyHotkey('mod+shift+enter');
 
+/**
+ * Handles key-down events and applies different mark and node-types
+ * based on the keys pressed. Offloads enter and backspace to
+ * `onEnter` and `onBackspace`.
+ */
 export function onKeyDown(event: any, editor: Editor, next: () => any) {
   let mark;
 
