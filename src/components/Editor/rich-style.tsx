2cc996d4b36fe4e98b5fc3a03bca4442e0140403
Christopher Brown <chbphone55@gmail.com>
2019-03-26 22:40:22 -0600

    working onnnn it

--- src/components/Editor/rich-style.tsx
+++ src/components/Editor/rich-style.tsx
@@ -1,63 +1,70 @@
-import Draft, { ContentBlock, DefaultDraftInlineStyle } from 'draft-js';
-import { Map } from 'immutable';
+// tslint:disable: ordered-imports
 import React from 'react';
+import { Node, Block } from 'slate';
 
-// @ts-ignore
-import PrismDecorator from 'draft-js-prism';
 import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-jsx';
 import 'prismjs/components/prism-typescript';
 import 'prismjs/components/prism-tsx';
-import Prism from 'prismjs';
+// import Prism from 'prismjs';
+
+// @ts-ignore
+import PluginEditCode from 'slate-edit-code';
+// @ts-ignore
+import PluginPrism from 'slate-prism';
 
 import 'prismjs/themes/prism-okaidia.css';
 
-export const inlineStyles: Array<{
-  label: string;
-  style: string;
-}> = [
-  { label: 'Bold', style: 'BOLD' },
-  { label: 'Italics', style: 'ITALIC' },
-  { label: 'Underline', style: 'UNDERLINE' },
-  { label: 'Code', style: 'CODE' },
-  { label: 'Strikethrough', style: 'STRIKETHROUGH' }
-];
+// export const styleMap = {
+//   ...DefaultDraftInlineStyle,
+//   BOLD: {
+//     fontWeight: '800'
+//   },
+//   STRIKETHROUGH: {
+//     textDecoration: 'line-through'
+//   },
+//   CODE: {
+//     backgroundColor: 'hsl(0, 0%, 95%)',
+//     fontSize: '1rem',
+//     fontFamily: '"Roboto Mono",Menlo,Monaco,"Courier New",Courier,monospace',
+//     borderRadius: '0.12rem'
+//   }
+// };
 
-export const styleMap = {
-  ...DefaultDraftInlineStyle,
-  BOLD: {
-    fontWeight: '800'
-  },
-  STRIKETHROUGH: {
-    textDecoration: 'line-through'
-  },
-  CODE: {
-    backgroundColor: 'hsl(0, 0%, 95%)',
-    fontSize: '1rem',
-    fontFamily: '"Roboto Mono",Menlo,Monaco,"Courier New",Courier,monospace',
-    borderRadius: '0.12rem'
-  }
-};
-
-const isOSX = navigator.platform.includes('Mac');
+const isMac = /Mac|iPod|iPhone|iPad/.test(navigator.platform);
 export function hasCommandModifier(event: React.KeyboardEvent<{}>): boolean {
-  return isOSX ? event.metaKey && !event.altKey : event.ctrlKey && !event.altKey;
+  return isMac && !event.altKey && (event.metaKey || event.ctrlKey);
 }
 
-export const decorator = new PrismDecorator({
-  prism: Prism,
-  defaultSyntax: 'tsx'
-});
-
-const CodeBlock = (props: any) => {
-  return <pre className="language-tsx Editor__code-block">{props.children}</pre>;
-};
-
-export const blockRenderMap = Draft.DefaultDraftBlockRenderMap.merge(
-  Map({
-    'code-block': {
-      element: 'pre',
-      wrapper: <CodeBlock />
-    }
+export const plugins = [
+  PluginPrism({
+    onlyIn: (block: Block) => block.type === 'code-block',
+    getSyntax: (block: Block) => block.data.get('syntax')
+  }),
+  PluginEditCode({
+    onlyIn: (block: Block) => block.type === 'code-block'
   })
-);
+];
+
+export function renderNode({ node, children, attributes, ...props }: any) {
+  switch (node.type) {
+    case 'code-block':
+      return (
+        <pre>
+          <code {...attributes}>{children}</code>
+        </pre>
+      );
+    case 'paragraph':
+      return <p {...attributes}>{children}</p>;
+    case 'heading':
+      return <h1 {...attributes}>{children}</h1>;
+    case 'blockquote':
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case 'unoredered-list-item':
+      return <li {...attributes}>{children}</li>;
+    case 'ordered-list-item':
+      return <ol {...attributes}>{children}</ol>;
+    default:
+      return null;
+  }
+}
