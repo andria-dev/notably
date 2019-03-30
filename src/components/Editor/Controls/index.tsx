6bac39dbdf9392c27c233c4180945a7e3e360760
Christopher Brown <chbphone55@gmail.com>
2019-03-30 02:12:54 -0600

    fix error

--- src/components/Editor/Controls/index.tsx
+++ src/components/Editor/Controls/index.tsx
@@ -1,4 +1,3 @@
-import { EditorState } from 'draft-js';
 import React, { useCallback } from 'react';
 
 import { types } from '../';
@@ -9,62 +8,59 @@ import classNames from '@chbphone55/classnames';
 import { useScrollYPosition } from 'react-use-scroll-position';
 import StyleButton from '../StyleButton';
 
+import { Value } from 'slate';
 import './style.css';
 
 const marks: [] = [];
 
 interface IControlsProps {
   dispatch: React.Dispatch<IAction>;
-  editorState: EditorState;
+  editorState: Value;
   className?: any;
 }
 function Controls({ dispatch, editorState, className, ...props }: IControlsProps) {
-  const currentInlineStyles = editorState.getCurrentInlineStyle();
-
-  const selection = editorState.getSelection();
-  const currentBlockStyle = editorState
-    .getCurrentContent()
-    .getBlockForKey(selection.getStartKey())
-    .getType();
-
-  const handleInline = useCallback(
-    (style: string) => {
-      dispatch({ type: types.INLINE, payload: style });
-    },
-    [dispatch]
-  );
-  const handleBlock = useCallback(
-    (type: string) => {
-      dispatch({ type: types.BLOCK, payload: type });
-    },
-    [dispatch]
-  );
-
-  const y = useScrollYPosition();
-  const raised = y >= 42;
-
-  return (
-    <section
-      className={classNames(
-        'Controls',
-        { 'shadow-md': raised, 'Controls--raised': raised },
-        className
-      )}
-      {...props}
-    >
-      {marks.map(({ label, style }) => (
-        <StyleButton
-          key={label}
-          onToggle={handleInline}
-          style={style}
-          active={currentInlineStyles.has(style)}
-          title={label}
-        >
-          {label}
-        </StyleButton>
-      ))}
-    </section>
-  );
+  // const currentInlineStyles = editorState.getCurrentInlineStyle();
+  // const selection = editorState.getSelection();
+  // const currentBlockStyle = editorState
+  //   .getCurrentContent()
+  //   .getBlockForKey(selection.getStartKey())
+  //   .getType();
+  // const handleInline = useCallback(
+  //   (style: string) => {
+  //     dispatch({ type: types.INLINE, payload: style });
+  //   },
+  //   [dispatch]
+  // );
+  // const handleBlock = useCallback(
+  //   (type: string) => {
+  //     dispatch({ type: types.BLOCK, payload: type });
+  //   },
+  //   [dispatch]
+  // );
+  // const y = useScrollYPosition();
+  // const raised = y >= 42;
+  // return (
+  //   <section
+  //     className={classNames(
+  //       'Controls',
+  //       { 'shadow-md': raised, 'Controls--raised': raised },
+  //       className
+  //     )}
+  //     {...props}
+  //   >
+  //     {marks.map(({ label, style }) => (
+  //       <StyleButton
+  //         key={label}
+  //         onToggle={handleInline}
+  //         style={style}
+  //         active={currentInlineStyles.has(style)}
+  //         title={label}
+  //       >
+  //         {label}
+  //       </StyleButton>
+  //     ))}
+  //   </section>
+  // );
 }
 
 export default Controls;
