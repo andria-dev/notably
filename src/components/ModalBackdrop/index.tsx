ea5d63d4c1778157b0e9b15c6e5703f3ceb30d54
Christopher Brown <chbphone55@gmail.com>
2019-04-15 20:17:40 -0600

    clicks on ModalBackdrop will only be passed up if direct

--- src/components/ModalBackdrop/index.tsx
+++ src/components/ModalBackdrop/index.tsx
@@ -1,9 +1,27 @@
 import classNames from '@chbphone55/classnames';
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import './style.css';
 
-function ModalBackdrop({ className, ...props }: any) {
-  return <div className={classNames('ModalBackdrop', className)} {...props} />;
+function ModalBackdrop({ className, onClick, ...props }: any) {
+  const ref = useRef(null);
+
+  const handleClick = useCallback(
+    event => {
+      if (ref.current === event.target) {
+        onClick(event);
+      }
+    },
+    [onClick]
+  );
+
+  return (
+    <div
+      ref={ref}
+      onClick={handleClick}
+      className={classNames('ModalBackdrop', className)}
+      {...props}
+    />
+  );
 }
 
 export default ModalBackdrop;
