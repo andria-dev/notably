152281360c1b4e6c3f2d27b0e93e99e2e517bfb8
Christopher Brown <chbphone55@gmail.com>
2019-04-25 12:56:57 -0600

    blep

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React, { memo, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  RouteComponentProps,
+  Switch
+} from 'react-router-dom';
+
+import { animated } from 'react-spring';
+import { useTransition } from './hooks';
 
 import Home from './pages/home';
 import Note from './pages/note';
@@ -12,20 +20,34 @@ import { DarkModeContext } from './contexts';
 /* Global CSS + Utilities */
 import './App.css';
 
+type Location = RouteComponentProps['location'];
+
 const themeMetaTags: NodeListOf<HTMLMetaElement> = document.querySelectorAll(`
   meta[name="theme-color"],
   meta[name="msapplication-navbutton-color"],
   meta[name="apple-mobile-web-app-status-bar-style"]
 `);
 
-const App = memo(() => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/note/:id" component={Note} />
-    </Switch>
-  </Router>
-));
+const App = memo(() => {
+  const transition = useTransition(
+    [location],
+    (currentLocation: Location) => currentLocation.pathname,
+    {
+      from: { opacity: 0 },
+      enter: { opacity: 1 },
+      leave: { opacity: 0 }
+    }
+  );
+
+  return transition.map(({ item, key, props }) => (
+    <animated.div style={props} key={key}>
+      <Switch location={item}>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/note/:id" component={Note} />
+      </Switch>
+    </animated.div>
+  ));
+});
 
 function AppWrapper() {
   const darkMode = useDarkMode(false, {
@@ -51,7 +73,9 @@ function AppWrapper() {
   return (
     <ReduxProvider store={store}>
       <DarkModeContext.Provider value={darkMode}>
-        <App />
+        <Router>
+          <App />
+        </Router>
       </DarkModeContext.Provider>
     </ReduxProvider>
   );
